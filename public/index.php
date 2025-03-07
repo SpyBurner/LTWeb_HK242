af55e97170290734e54935d262f2da9bd4a74dda
nialliceh@gmail.com <nialliceh@gmail.com>
2025-03-07 17:48:19 +0700

    before switching to penguinui

---
+++ public/index.php
@@ -0,0 +1,25 @@
+<?php
+
+session_start();
+
+$route = isset($_GET['route']) ? trim($_GET['route'], '/') : '';
+
+// echo $route;
+
+if (empty($route)) {
+    require_once(__DIR__.'/../app/view/home/home.php');
+}
+else {
+    switch($route) {
+        case 'contact':
+            require_once(__DIR__.'/../app/view/contact/contact.php');
+            break;
+        default:
+            require_once(__DIR__.'/../app/view/404.php');
+            break;
+    }
+}
+
+
+
+?>
