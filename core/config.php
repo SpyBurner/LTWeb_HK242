62da4bd0a1b797dde8fb648d5221142a33bdce04
SpyBurner <zombiepigman1505@gmail.com>
2025-05-01 11:04:55 +0700

    db port .env

--- core/config.php
+++ core/config.php
@@ -13,6 +13,7 @@ $env = parse_ini_file($env_file);
 
 // Define constants
 define('DB_HOST', $env['DB_HOST']);
+define('DB_PORT', $env['DB_PORT']);
 define('DB_NAME', $env['DB_NAME']);
 define('DB_USER', $env['DB_USER']);
 define('DB_PASS', $env['DB_PASS']);
